615dd3cf41d507fed2b692d02c3a1b6dac860a65
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-09 19:48:20 +0200

    Add logic for fetching data from APIs

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io?name=${name}`);
+  return res.json();
+};
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
+
+const getPredictedNationality = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
 interface Params {
   params: { name: string };
 }
 
-export default function Page({ params }: Params) {
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const nationalityData = getPredictedNationality(params.name);
+
+  const [age, gender, nationality] = await Promise.all([
+    ageData,
+    genderData,
+    nationalityData,
+  ]);
+
   return (
     <div>
       <p>name</p>
