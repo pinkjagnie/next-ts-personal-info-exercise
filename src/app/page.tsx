47b04c603434f02ddb7b74ecd56c516a48ad6cb8
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-09 19:14:16 +0200

    Add form /w styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hi</p>
-    </main>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Enter Your Name
+        </h1>
+        <form className="space-y-3">
+          <input
+            type="text"
+            placeholder="Type your name"
+            className="w-full p-2 border border-gray-300 rounded text-black"
+          />
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
